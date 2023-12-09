79f7c34c6539b8de77a8cc81361a962b15747af1
TheTechnikfreak <paulschmid511+spam@gmail.com>
2023-12-09 12:04:18 +0100

    change

--- apache/form.php
+++ apache/form.php
@@ -3,7 +3,7 @@
 <?php
 if($_POST['pw'] == "Geheim") {
 } else {
-   echo '<p>Passwort ist falsch. Sie werden in 2 Sekunden zurück geleitet.</p>';
+   echo '<p>Passwort ist falsch. Sie werden in 2 Sekunden zurÃ¼ck geleitet.</p>';
    header("Refresh: 2; url=index.html");
    exit();
    
@@ -18,13 +18,14 @@ if($_POST['pw'] == "Geheim") {
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
    </head>
    <body>
-      <form action="insert.php?alarm=on"  method="post">
+      <form action="http://schulsani.fritz.box:1880/data"  method="post">
         Raum(max.Zeichen 15)<br>
-        <input type="text" maxlength="15" name="field1"><br>
+        <input type="text" maxlength="15" name="raum"><br>
         Unfall(max.Zeichen 13)<br>
-        <input type="text" maxlength="13" name="field2"><br>
+        <input type="text" maxlength="13" name="unfall"><br>
         <input type="hidden" name="alarm" value="on">
          <input type="submit" value="Submit">
       </form>
    </body>
   </html>
+
